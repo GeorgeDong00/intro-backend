de5cc6212b536c88baf2b658e6361adb17eac8cb
George Dong <georgedong0501@gmail.com>
2022-08-04 18:02:17 -0400

    add sql methods to initialize, delete, and insert into database

--- pa2-venmo-style-sql/src/db.py
+++ pa2-venmo-style-sql/src/db.py
@@ -15,9 +15,81 @@ def singleton(cls):
 
 class DatabaseDriver(object):
     """
-    Database driver for the Task app.
+    Database driver for the transaction app.
     Handles with reading and writing data with the database.
     """
 
     def __init__(self):
-        pass
+        """
+        Initializes database connection and create SQL table.
+        """
+        self.conn = sqlite3.connect("venmo.db", check_same_thread = False)
+        self.create_users_table()
+    
+    def create_users_table(self):
+        """
+        Using SQL, creates a user database that tracks id, name, username, and
+        balance.
+        """
+        try:
+            self.conn.execute(
+                """
+                CREATE TABLE users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    username TEXT NOT NULL,
+                    balance INTEGER NOT NULL
+                );
+                """
+            )
+        except Exception as e:
+            print(e)
+        
+    def delete_users_table(self):
+        """
+        Using SQL, delete entire table
+        """
+        self.conn.execute("DROP TABLE IF EXISTS users;")
+
+    def select_users(self):
+        """
+        Using SQL, return a list with dictionary of each user's id, name and
+        username.
+        """
+        cursor = self.conn.execute("SELECT * FROM users")
+        users = []
+        for row in cursor:
+            users.append({"id": row[0], "name": row[1], "username": row[2]})
+        return users
+    
+    def insert_user(self, name, username, balance):
+        """
+        Using SQL, insert new user into the table 
+
+        Parameter name, username, balance are parameter that accepts the 
+        user's information.
+        """
+        cursor = self.conn.execute(
+            """
+            INSERT INTO users (name, username, balance) 
+            VALUES (?, ?, ?);
+            """, (name, username, balance))
+        # commit the sql insertion
+        self.conn.commit()
+        # return last inserted row
+        return cursor.lastrowid
+    
+    def select_user_id(self, id):
+        """
+        Return list containing information about user of specified id
+
+        Parameter id refers to user's id. 
+        Precondition id should exist in users table. 
+        """
+        cursor = self.conn.execute("SELECT * FROM users WHERE id = ?;", (id,))
+        user = []
+        for row in cursor:
+            user.append({"id": row[0], "name": row[1], "username": row[2], "balance": row[3]})
+        return user
+    
+
