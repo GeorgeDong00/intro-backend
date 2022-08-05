59db31f74f90c9ec38c8a10054a6c1a69d6d379e
George Dong <georgedong0501@gmail.com>
2022-08-04 22:20:24 -0400

    add endpoints to get specific user, delete user and transfer money

--- pa2-venmo-style-sql/src/app.py
+++ pa2-venmo-style-sql/src/app.py
@@ -26,8 +26,10 @@ def get_users():
 @app.route("/api/users/", methods=["POST"])
 def create_user():
     """
-    Endpoint returns json of information on newly created user and 
-    success code 201
+    Endpoint creates new user using request body information containing name,
+    username and optional balance amount (otherwise 0).
+    
+    Returns json of information on newly created user and success code 201. 
     """
     # fetch body information
     body = json.loads(request.data)
@@ -42,6 +44,73 @@ def create_user():
         return json.dumps(DB.select_user_id(user_id)), 201
 
     return json.dumps({"error": "Name and/or username missing"}), 400
+
+@app.route("/api/user/<int:user_id>/", methods=["GET"])
+def get_user(user_id):
+    """
+    Endpoint fetches user of specified id. 
+
+    Returns json of user's information if successful.
+    """
+    user_data = DB.select_user_id(user_id)
+    if user_data is not None:
+        return json.dumps(DB.select_user_id(user_id)), 201
+    return json.dumps({"error": "User doesn't exist"}), 404
+
+@app.route("/api/user/<int:user_id>/", methods=["DELETE"])
+# TODO: debug del_user so it returns error if id doesn't exist
+def delete_user(user_id):
+    """
+    Endpoint deletes user of specified id.
+
+    Returns json of deleted user's information if successful. 
+    """
+    del_user = DB.select_user_id(user_id)
+    if del_user is None:
+        return json.dumps({"error": "User doesn't exist"}), 404
+    DB.delete_user(user_id)
+    return json.dumps(del_user), 200
+    
+
+@app.route("/api/send/", methods=["POST"])
+def send_money():
+    """
+    Endpoint that accepts a request with sender's id, receiver's id and 
+    transfer amount and initiates the transaction.
+    
+    Returns request body of transaction process if successful.
+    """
+    # validate sender_id, receiver_id, amount parameter are filled
+    body = json.loads(request.data)
+    print(body)
+    if body["sender_id"] and body["receiver_id"] and body["amount"]: 
+
+        sender = body.get("sender_id")
+        receiver = body.get("receiver_id")
+        amount = body.get("amount")
+        
+        # validate sender_id and receiver_id exist in database
+        sender_data = DB.select_user_id(sender)
+        receiver_data = DB.select_user_id(receiver)
+
+        if (sender_data is not None) and (receiver_data is not None):
+            # validate sender's balance exceeds amount being sent
+            if sender_data[0]["balance"] >= body["amount"]:
+
+                # deduct sender_id's balance by amount
+                send_bal = sender_data[0]["balance"] - amount
+                DB.update_bal(sender, send_bal)
+
+                # increase receiver_id's balance by amount
+                receiver_bal = receiver_data[0]["balance"] + amount
+                DB.update_bal(receiver, receiver_bal)
+
+                #return json of transaction
+                return json.dumps(body), 200
+
+            return json.dumps({"error": "Sender's amount exceed balance"}), 400
+        return json.dumps({"error": "Sender and/or receiver doesn't exist"}), 404
+    return json.dumps({"error": "Missing sender, receiver, and/or transaction amount"}), 400
 
 
 
